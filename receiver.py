3fc00f01d5f6c0a67dbe98c8570239a84f51ad05
johnkim6823 <82578738+johnkim6823@users.noreply.github.com>
2022-09-20 15:51:00 +0900

    Create receiver.py

---
+++ receiver.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# http://weifan-tmm.blogspot.kr/2015/07/a-simple-turorial-for-python-c-inter.html
+import sysv_ipc
+import struct
+
+from type_definitions import *
+SIZEOF_FLOAT = 8
+
+try:
+    mq = sysv_ipc.MessageQueue(1234, sysv_ipc.IPC_CREAT)
+
+    while True:
+        message, mtype = mq.receive()
+        print("*** New message received ***")
+        print(f"Raw message: {message}")
+        
+        if mtype == TYPE_CAMERA_CFG:
+            camera_cfg_recv = struct.unpack("BBi", message)
+            print(f"Interpret as BBi: {camera_cfg_recv}")
+
+except sysv_ipc.ExistentialError:
