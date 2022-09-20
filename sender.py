a626c856b4fb9d367ad537781045a3f9088f0725
johnkim6823 <82578738+johnkim6823@users.noreply.github.com>
2022-09-20 15:06:50 +0900

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # http://weifan-tmm.blogspot.kr/2015/07/a-simple-turorial-for-python-c-inter.html
 import sysv_ipc
-import numpy as np
 import struct
 
 BUFF_SIZE = 8
@@ -11,18 +10,17 @@ from type_definitions import *
 
 if __name__ == '__main__':
         
-    msg_size = "VGA\0"
+    msg_size = VGA
     msg_fps = 30
-    msg_format = "YUV420\0"
+    msg_format = YUV420
     
     try:
         mq = sysv_ipc.MessageQueue(1234, sysv_ipc.IPC_CREAT)
         
         # by HEADER
-        header_size = len(msg_size) + INT_SIZE + len(msg_format)
-        header = bytearray(header_size)
-        fmt_str = f"<{len(msg_size)}si{len(msg_format)}s" 
-        struct.pack_into(fmt_str, header, 0, bytes(msg_size, 'utf-8'), msg_fps, bytes(msg_format, 'utf-8')) 
+        header = bytearray(HEADER_SIZE)
+        fmt_str = "<BBi" 
+        struct.pack_into(fmt_str, header, 0, msg_size, msg_format, msg_fps) 
         print (header)
         mq.send(header, True, type=TYPE_CAMERA_CFG)
 
